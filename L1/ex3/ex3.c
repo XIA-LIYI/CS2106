29859c6d8e8f4553599a3bb32ded22f5fb976ae0
XIA-LIYI-Ubuntu <xialiyi@DESKTOP-8JH1F5U.localdomain>
2021-02-02 14:31:59 +0800

    Finish ex3

--- L1/ex3/ex3.c
+++ L1/ex3/ex3.c
@@ -13,7 +13,10 @@ typedef int (*arithFuncPtr)(int, int);
 
 //Function Prototypes
 int add(int x, int y);
-
+int spell1(int x, int y);
+int spell2(int x, int y);
+int spell3(int x, int y);
+int spell4(int x, int y);
 
 int main()
 {
@@ -25,27 +28,44 @@ int main()
     //arithFuncPtr is a function pointer datatype
     //   - it points to afunction that takes (int, int) as parameter
     //   - and returns int as return result
+    
+    arithFuncPtr actionList[4] = {spell1, spell2, spell3, spell4}; 
+    char nameList[4][13] = {"lumos", "alohomora", "expelliarmus", "sonorus"};
 
-    arithFuncPtr ptr;
-
-    //ptr points to the function add
-    ptr = add;
-
-    scanf("%i %i", &a, &b);
-
-    //Defereference a function pointer === call the function
-    //if you take (*ptr) as some function f
-    // then below is the same as
-    //   res = f(a, b);
-
-    res = (*ptr)(a, b);
-
-    printf("%i\n", res);
+    while (scanf("%i", &optype) == 1) {
+    	scanf("%i %i", &a, &b);
+	res = actionList[optype - 1](a, b);
+    	printf("%s %i\n", nameList[optype - 1], res);
+    }
    
     return 0;
 }
 
-int add(int x, int y)
-{
-    return x+y;
+int spell1(int x, int y) {
+    return (x + y) * (y - x + 1) / 2;
+}
+
+int spell2(int x, int y) {
+    int gcd = 1;
+    for (int i = 1; i <= x && i <= y; i++) {
+    	if (x % i == 0 && y % i == 0) {
+	    gcd = i;
+	}
+    }
+    return gcd;
+}
+
+int spell3(int x, int y) {
+    while (x % y == 0) {
+    	x = x / 3;
+    }
+    return x;
+}
+
+int spell4(int x, int y) {
+    int res = 1;
+    for (int i = 1; i < y + 1; i++) {
+    	res = res * x;
+    }
+    return res;
 }
