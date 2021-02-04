1563e8efdd81ae0fdf4695e3872e08b33651f9c8
XIA <xialiyi110@gmail.com>
2021-02-04 16:58:58 +0800

    Revise the submission

--- L1/ex3/ex3.c
+++ L1/ex3/ex3.c
@@ -1,71 +1,76 @@
 /*************************************
 * Lab 1 Exercise 3
-* Name     :
-* Matric No:
-* Lab Group:
+* Name     : Xia Liyi
+* Matric No: A0177873L
+* Lab Group: B19
 *************************************/
 
+/*************************************
+* Lab 1 Exercise 3
+* Name     : Zhang Yunjie
+* Matric No: A0204208U
+* Lab Group: B09
+*************************************/
+
+
 #include <stdio.h>
+#include <math.h>
 
 //Datatype Declarations
 typedef int (*arithFuncPtr)(int, int);
 
 
 //Function Prototypes
-int add(int x, int y);
-int spell1(int x, int y);
-int spell2(int x, int y);
-int spell3(int x, int y);
-int spell4(int x, int y);
+int ap(int x, int y);
+int gcd(int x, int y);
+int removeFactor(int x, int y);
+int power(int x, int y);
 
-int main()
-{
+int main() {
     int a, b, optype, res;
 
-    //The code below is to show the usage of a function pointer only
-    //You can remove/comment them off 
-
-    //arithFuncPtr is a function pointer datatype
+//arithFuncPtr is a function pointer datatype
     //   - it points to afunction that takes (int, int) as parameter
     //   - and returns int as return result
-    
-    arithFuncPtr actionList[4] = {spell1, spell2, spell3, spell4}; 
-    char nameList[4][13] = {"lumos", "alohomora", "expelliarmus", "sonorus"};
 
-    while (scanf("%i", &optype) == 1) {
-    	scanf("%i %i", &a, &b);
-	res = actionList[optype - 1](a, b);
-    	printf("%s %i\n", nameList[optype - 1], res);
+
+    //ptr points to the function add
+    ptr1 = ap;
+    ptr2 = gcd;
+    ptr3 = removeFactor;
+    ptr4 = power;
+
+    arithFuncPtr ptrArray[4] = {ap,gcd,removeFactor,power};
+    char printArray[4][13] = {"lumos", "alohomora", "expelliarmus", "sonorus"};
+    
+    while (scanf("%i %i %i", &optype, &a, &b) == 3) {
+        res = (*ptrArray[optype - 1])(a, b);
+        printf("%s %i\n", printArray[optype - 1], res);
     }
-   
+    
     return 0;
 }
 
-int spell1(int x, int y) {
+
+int gcd(int x, int y) {
+    if (y != 0) {
+        return gcd(y, x % y);
+    } else {
+        return x;
+    }
+}
+
+int power(int base, int power) {
+    return pow(base,power);
+}
+
+int ap(int x, int y) { //arithmetic progression
     return (x + y) * (y - x + 1) / 2;
-}
+} 
 
-int spell2(int x, int y) {
-    int gcd = 1;
-    for (int i = 1; i <= x && i <= y; i++) {
-    	if (x % i == 0 && y % i == 0) {
-	    gcd = i;
-	}
+int removeFactor(int num, int factor) {
+    while (num % factor == 0) {
+        num /= factor;
     }
-    return gcd;
-}
-
-int spell3(int x, int y) {
-    while (x % y == 0) {
-    	x = x / 3;
-    }
-    return x;
-}
-
-int spell4(int x, int y) {
-    int res = 1;
-    for (int i = 1; i < y + 1; i++) {
-    	res = res * x;
-    }
-    return res;
+    return num;
 }
